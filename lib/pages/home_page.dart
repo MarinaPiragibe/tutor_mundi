e65935ad460fcd28df2338d44428f9c2e26da418
MarinaPiragibe <marinapiragibe@id.uff.br>
2023-02-13 16:27:46 -0300

    Implementa a mudança de coresno botão de confirmar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,28 +10,39 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   double _avaliacao = 0;
-  int _ratingBarMode = 0; //1
-  bool _isButtonDisabled = true;
+  final int _ratingBarMode = 0; //1
 
+  bool _isButtonDisabled = true; // Controla a ativação do botão de confirmar
+  Color _confirmButton_color = Colors.grey;
+
+  // Método de criação das estrelas de avaliação com o ratingBar Widget
   Widget _ratingBar(int mode) {
     return RatingBar(
       initialRating: 0,
       allowHalfRating: false,
-      itemCount: 5,
+      itemCount: 5, // Admite o valor de 5 estrelas
+
       ratingWidget: RatingWidget(
         full: Image.asset('assets/images/estrela_cheia.png'),
         half: Image.asset('assets/images/estrela_cheia.png'),
         empty: Image.asset('assets/images/estrela_vazia.png'),
       ),
-      itemPadding: EdgeInsets.symmetric(horizontal: 4),
+
+      itemPadding: const EdgeInsets.symmetric(horizontal: 4),
+
+      // Função responsável por atualizar o valor da avaliação de acordo com a estrela selecionada
       onRatingUpdate: (avaliacao) {
         setState(() {
           _avaliacao = avaliacao;
         });
         if (_avaliacao == 0) {
+          // Desabilita o botão de confirmar caso nenhuma estrela tenha sido selecionada
           _isButtonDisabled = true;
+          _confirmButton_color = Colors.grey;
         } else {
+          // Caso contrário, habilita o botão de confirmar
           _isButtonDisabled = false;
+          _confirmButton_color = Colors.blue;
         }
       },
     );
@@ -42,15 +53,15 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         body: Center(
             child: Container(
-                padding: EdgeInsets.all(16),
-                margin: EdgeInsets.all(16),
+                padding: const EdgeInsets.all(16),
+                margin: const EdgeInsets.all(16),
                 child: Column(children: [
                   Image.asset(
                     "assets/images/livro.png",
                     fit: BoxFit.fill,
                     alignment: Alignment.center,
                   ),
-                  Text(
+                  const Text(
                     'Como foi a ajuda do tutor?',
                     style: TextStyle(
                         fontSize: 34,
@@ -61,29 +72,39 @@ class _HomePageState extends State<HomePage> {
                   _ratingBar(_ratingBarMode),
                   Text(
                     "Avaliação: $_avaliacao",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                    style: const TextStyle(fontWeight: FontWeight.bold),
                   ),
-                  TextButton(
-                    child: Text(
-                      'PULAR',
-                      style: TextStyle(
-                          color: Colors.blue,
-                          fontFamily: 'WorkSans',
-                          fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: null,
-                  ),
-                  FloatingActionButton.extended(
-                    label: Text('CONFIRMAR',
-                        style: TextStyle(
-                            fontFamily: 'WorkSans',
-                            fontWeight: FontWeight.bold)),
-                    backgroundColor: Colors.grey,
-                    onPressed: _isButtonDisabled
-                        ? null
-                        : () {
-                            print(_avaliacao);
-                          },
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const TextButton(
+                        onPressed: null,
+                        child: Text(
+                          'PULAR',
+                          style: TextStyle(
+                              color: Colors.blue,
+                              fontFamily: 'WorkSans',
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 143,
+                        height: 40,
+                        child: FloatingActionButton.extended(
+                          label: const Text('CONFIRMAR',
+                              style: TextStyle(
+                                  fontFamily: 'WorkSans',
+                                  fontWeight: FontWeight.bold)),
+                          backgroundColor: _confirmButton_color,
+                          onPressed: _isButtonDisabled
+                              ? null
+                              : () {
+                                  print(_avaliacao);
+                                },
+                        ),
+                      ),
+                    ],
                   ),
                 ]))));
   }
